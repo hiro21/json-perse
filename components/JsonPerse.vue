f760aad186e0980631335f081a0a0d63e6b24777
hiroyuki-seto <hiswing8@gmail.com>
2022-05-06 11:52:32 +0900

    onclickイベントを追加

--- components/JsonPerse.vue
+++ components/JsonPerse.vue
@@ -4,6 +4,17 @@
       <textarea class="form-control" id="perseJsonTextarea" rows="3"></textarea>
       <label for="perseResultTextarea" class="form-label">after json text</label>
       <textarea class="form-control" id="perseResultTextarea" rows="3"></textarea>
-      <button type="button" class="btn btn-dark">submit</button>
+      <button type="button" class="btn btn-dark" @click="onClickEventPerse">submit</button>
     </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    methods: {
+        onClickEventPerse(event: Event): void {
+            console.log("test")
+        }
+    }
+})
+
+</script>
